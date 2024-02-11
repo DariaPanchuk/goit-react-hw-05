bea957bb23aad1c5e2e0906a7b582d2ea4cbdb6a
DariaPanchuk <daria_panchuk@ukr.net>
2024-02-11 15:20:23 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -11,7 +11,7 @@ export const fetchTrendingMovies = async () => {
 };
 
 export const fetchByQuery = async (query, page) => {
-    const url = `search/movie?query=${query}&include_adult=false&language=en-US&${page}`;
+    const url = `search/movie?query=${query}&include_adult=false&language=en-US&page=${page}`;
     
     const response = await axios.get(url);
     return response.data;
